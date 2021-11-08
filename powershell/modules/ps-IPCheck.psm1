a87fec6fecd15a955b58a38af1f0a29754d76aca
Marcobrmn <91803571+Marcobrmn@users.noreply.github.com>
2021-11-08 21:45:00 +0100

    Delete ps-IPCheck.psm1

--- powershell/modules/ps-IPCheck.psm1
+++
@@ -1,8 +0,0 @@
-Function ps-IPCheck {
-    Param ($IPaddress)
-    if (!$IPaddress) {
-        $IPaddress = Read-Host "Please enter local IP to check"
-    }
-    $localIPaddress = (Get-NetIPAddress).ipaddress
-    $localIPaddress -contains $IPaddress
-}
